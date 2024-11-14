1bedd66f8fd1b181498341a5fc9266f9a4172815
harshitg187@gmail.com <harshitg187@gmail.com>
2024-11-15 00:26:43 +0530

    PORT

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,3 +20,7 @@ from app import routes, models
 
 with app.app_context():
     db.create_all()
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
