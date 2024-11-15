693cace9789a55d0237f43f13e335d2ffad08356
harshitg187@gmail.com <harshitg187@gmail.com>
2024-11-15 15:38:25 +0530

    fixes

--- app/routes.py
+++ app/routes.py
@@ -142,7 +142,7 @@ def signup():
 
     except Exception as e:
         db.session.rollback()
-        return jsonify({'message': 'Error creating user', 'error': str(e)}), 500
+        return jsonify({'message': f'Error creating user {str(e)}', 'error': str(e)}), 500
     
 @app.route('/login', methods=['POST'])
 def login():
