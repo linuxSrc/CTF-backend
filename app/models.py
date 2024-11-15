836b986e5e0a2dd0a42dcde8ec91358194d98ae4
harshitg187@gmail.com <harshitg187@gmail.com>
2024-11-15 15:46:45 +0530

    MAJOR FIXES

--- app/models.py
+++ app/models.py
@@ -30,7 +30,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
     challenges = db.relationship('Challenge', secondary='user_challenge', overlaps="users")
     total_score = db.Column(db.Integer, default=0)
     
